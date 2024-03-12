a6ca7264ea2cb336602568e8c2b383e585b7818b
Royweru <royezi17@gmail.com>
2024-03-12 21:56:42 +0300

    new file:   public/assets/icons/add-post.svg 	new file:   public/assets/icons/back.svg 	new file:   public/assets/icons/bookmark.svg 	new file:   public/assets/icons/chat.svg 	new file:   public/assets/icons/delete.svg 	new file:   public/assets/icons/edit.svg 	new file:   public/assets/icons/favicon.ico 	new file:   public/assets/icons/file-upload.svg 	new file:   public/assets/icons/filter.svg 	new file:   public/assets/icons/follow.svg 	new file:   public/assets/icons/gallery-add.svg 	new file:   public/assets/icons/google.svg 	new file:   public/assets/icons/home.svg 	new file:   public/assets/icons/like.svg 	new file:   public/assets/icons/liked.svg 	new file:   public/assets/icons/loader.svg 	new file:   public/assets/icons/logout.svg 	new file:   public/assets/icons/people.svg 	new file:   public/assets/icons/posts.svg 	new file:   public/assets/icons/profile-placeholder.svg 	new file:   public/assets/icons/save.svg 	new file:   public/assets/icons/saved.svg 	new file:   public/assets/icons/search.svg 	new file:   public/assets/icons/share.svg 	new file:   public/assets/icons/wallpaper.svg 	modified:   public/assets/images/logo.svg 	new file:   public/assets/images/profile.png 	new file:   public/assets/images/side-img.svg 	deleted:    public/vite.svg 	modified:   src/components/shared/bottom-bar.tsx 	modified:   src/constants/index.ts

--- src/components/shared/bottom-bar.tsx
+++ src/components/shared/bottom-bar.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+
+import { useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { bottombarLinks } from '@/constants'
+import { INavLink } from '@/types'
 
 export const BottomBar = () => {
+  const pathName =useLocation()
+
   return (
-    <div>BottomBar</div>
+    <section>
+      {bottombarLinks.map((link:INavLink)=>{
+      const isActive = pathName===link.route
+      return(
+          <li key={link.label}
+           className={`leftsidebar-link ${isActive && 'bg-primary-500 rounded-[10px]'} 
+           flex-center flex-col gap-1 p-2 transition`}
+          >
+            <Link to={link.route}>
+               <img src={link.imgURL}  alt={link.label}  
+                 className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+               />
+                  <img src={link.imgURL}  alt={link.label} width={16} height={16} className={`${isActive && 'invert-white'}`} />
+                  <p className=' tiny-medium text-light-2'>
+              {link.label}
+            </p>
+            </Link>     
+            
+          </li>
+      )
+     })}
+     </section>
   )
 }
